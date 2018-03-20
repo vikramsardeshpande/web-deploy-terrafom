788ab03653412862e5d25310eebeef1f3266cf77
vikram sardeshpande <vikram@pspl.com>
2018-03-20 00:50:39 +0000

    deplyed lb + instance

--- web-deploy/web/instance.tf
+++ web-deploy/web/instance.tf
@@ -7,7 +7,7 @@ provider "aws" {
 data "terraform_remote_state" "vpc" {
   backend = "local"
   config {
-    path = "/home/vikram-demo/web-deploy-project/vpc/terraform.tfstate"
+    path = "../vpc/terraform.tfstate"
   }
 }
 #resource "aws_instance" "bastion-1b" {
@@ -26,6 +26,30 @@ resource "aws_instance" "bastion-1d" {
         security_groups = ["${aws_security_group.web.id}"]
         key_name = "mykey"
         associate_public_ip_address = true
+     #   user_data= "#!/bin/bash\n yum update -y \n yum install httpd -y \n service httpd restart"
+	provisioner "file" {
+    	source = "index.html"
+       	destination = "/tmp/index.html"
+         }
+	provisioner "remote-exec" {
+  	  inline = [
+          "sudo yum update -y",
+          "sudo yum install httpd -y",
+          "sudo service httpd restart", 
+    	  "sudo chmod +x /tmp/index.html",
+	  "sudo chmod 777 /var/www/html",
+       	  "/usr/bin/sudo /bin/cp /tmp/index.html /var/www/html/index.html"
+  	  ]
+ 	 }
+    	connection { 
+	user = "${var.INSTANCE_USERNAME}"
+        private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+        }
+	tags {
+             Name = "demo v1"
+            }
+ 
+	
 }
 
 # security group for web 
@@ -65,3 +89,77 @@ resource "aws_security_group" "web" {
     Name = "web-project-example"
   }
 }
+# Our elb security group to access 
+# the ELB over HTTP 
+ resource "aws_security_group" "elb" { 
+   name        = "elb_sg" 
+   description = "Used in the terraform" 
+ 
+ 
+   vpc_id = "${data.terraform_remote_state.vpc.vpc_id}" 
+ 
+ 
+   # HTTP access from anywhere 
+   ingress { 
+     from_port   = 80 
+     to_port     = 80 
+     protocol    = "tcp" 
+     cidr_blocks = ["0.0.0.0/0"] 
+   } 
+ 
+ 
+   # outbound internet access 
+  egress { 
+     from_port   = 0 
+     to_port     = 0 
+     protocol    = "-1" 
+     cidr_blocks = ["0.0.0.0/0"] 
+   } 
+ 
+ 
+ # ensure the VPC has an Internet gateway or this step will fail 
+#   depends_on = ["aws_internet_gateway.gw"] 
+ } 
+resource "aws_elb" "web" { 
+   name = "example-elb" 
+ 
+ 
+   # The same availability zone as our instance 
+  subnets = ["${data.terraform_remote_state.vpc.public_subnet_id_1d}"] 
+  # subnet_id = "${data.terraform_remote_state.vpc.public_subnet_id_1d}" 
+ 
+   security_groups = ["${aws_security_group.elb.id}"] 
+ 
+ 
+   listener { 
+     instance_port     = 80 
+     instance_protocol = "http" 
+     lb_port           = 80 
+     lb_protocol       = "http" 
+   } 
+ 
+ 
+   health_check { 
+     healthy_threshold   = 2 
+     unhealthy_threshold = 2 
+     timeout             = 3 
+     target              = "HTTP:80/" 
+     interval            = 30 
+   } 
+  # The instance is registered automatically 
+ 
+ 
+   instances                   = ["${aws_instance.bastion-1d.id}"] 
+   cross_zone_load_balancing   = true 
+   idle_timeout                = 400 
+   connection_draining         = true 
+   connection_draining_timeout = 400 
+ } 
+ 
+ 
+ resource "aws_lb_cookie_stickiness_policy" "default" { 
+   name                     = "lbpolicy" 
+   load_balancer            = "${aws_elb.web.id}" 
+   lb_port                  = 80 
+   cookie_expiration_period = 600 
+ } 
--- web-deploy/web/vars.tf
+++ web-deploy/web/vars.tf
@@ -1,6 +1,9 @@
-#variable "PATH_TO_PRIVATE_KEY" {
-#  default = "mykey"
-#}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
 #variable "PATH_TO_PUBLIC_KEY" {
 #  default = "mykey.pub"
 #}
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
